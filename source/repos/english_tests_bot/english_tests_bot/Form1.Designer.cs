44edb0b2f939108f4f009c253f4132b2d7750ba2
anakib1 <b.svjatoslav@gmail.com>
2018-03-17 15:20:52 +0200

    first stable v

---
+++ source/repos/english_tests_bot/english_tests_bot/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace english_tests_bot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.token_box = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(109, 212);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "start!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // token_box
+            // 
+            this.token_box.Location = new System.Drawing.Point(109, 116);
+            this.token_box.Name = "token_box";
+            this.token_box.Size = new System.Drawing.Size(100, 20);
+            this.token_box.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.token_box);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox token_box;
+    }
+}
+
---
+++ source/repos/english_tests_bot/english_tests_bot/Form1.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace english_tests_bot
+{
+    public class Brake
+    {
+        //Dictionary<string, string> words = new Dictionary<string, string>();
+        string[] ewords;
+        public string now_word;
+        public void SetDic(string[] w)
+        {
+            
+            ewords = w;
+            
+        }
+        public string w()
+        {
+            return ewords[0];
+        }
+        public char[] brakingC()
+        {
+            Random random = new Random();
+            int index = random.Next(0, ewords.Length);
+            now_word = ewords[index];
+            char[] res = ewords[index].ToCharArray();
+            char[] res2 = res.OrderBy(x => random.Next()).ToArray();
+            return res2;
+        }
+        public char[] brakingC(string worda)
+        {
+            Random random = new Random();
+            char[] res = worda.ToCharArray();
+            char[] res2 = res.OrderBy(x => random.Next()).ToArray();
+            return res2;
+        }
+        public bool GetRes(string res)
+        {
+            if(ewords.Contains(res.ToLower()))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+    public partial class Form1 : Form
+    {
+        BackgroundWorker bw;
+        public Form1()
+        {
+            this.bw = new BackgroundWorker();
+            this.bw.DoWork += this.bw_DoWork;
+            InitializeComponent();
+        }
+        async void bw_DoWork(object sender,DoWorkEventArgs e)
+        {
+            var worker = sender as BackgroundWorker;
+            Telegram.Bot.Types.User u = new Telegram.Bot.Types.User();
+            var key = e.Argument as String;
+            bool setwords = false;
+            bool settranslate = false;
+            string[] words=null;
+            string[] wordstranslate;
+            bool answer=false;
+            bool second_c = false;
+            string w=String.Empty;
+            int n_of_tests = 0;
+            bool test = false;
+            Brake brake = new Brake();
+            try
+            {
+                var Bot = new Telegram.Bot.TelegramBotClient(key);
+                await Bot.SetWebhookAsync("");
+                int offset = 0;
+                while(true)
+                {
+                    var updates = await Bot.GetUpdatesAsync(offset);
+                    foreach( var update in updates)
+                    {
+                        var message = update.Message;
+                        if (message == null)
+                            continue;
+                        if(message.Type==Telegram.Bot.Types.Enums.MessageType.TextMessage)
+                        {
+                            /*
+                            if (settranslate)
+                            {
+                                wordstranslate = message.Text.ToLower().Split(' ');
+                                settranslate = false;
+                                await Bot.SendTextMessageAsync(message.Chat.Id, "good, now you can practise!");
+                                try
+                                {
+                                    brake.SetDic(wordstranslate, words);
+                                }
+                                catch(Exception)
+                                {
+                                    await Bot.SendTextMessageAsync(message.Chat.Id, "error");
+
+                                }
+
+
+                            }*/
+                            if (setwords)
+                            {
+                                words = message.Text.ToLower().Split(' ');
+                                brake.SetDic(words);
+                                await Bot.SendTextMessageAsync(message.Chat.Id, "now you can start test");
+                                setwords = false;
+
+                            }
+                            if (message.Text.Contains("/starttest"))
+                            {
+                                test = true;
+
+                            }
+                            if (message.Text.Contains("/setwords"))
+                            {
+                                try
+                                {
+                                    setwords = true;
+                                    await Bot.SendTextMessageAsync(message.Chat.Id, "please insert words in one string");
+                                    test = false;
+                                    n_of_tests = 0;
+                                    answer = false;
+                                }
+                                catch(Exception)
+                                {
+                                    await Bot.SendTextMessageAsync(message.Chat.Id, "error");
+
+                                }
+                            }
+                            if(answer)
+                            {
+                                if(brake.GetRes(message.Text.ToLower()))
+                                {
+                                    await Bot.SendTextMessageAsync(message.Chat.Id, "yes, it`s true");
+                                    second_c = false;
+                                }
+                                else
+                                {
+                                    await Bot.SendTextMessageAsync(message.Chat.Id, "NO, try once more");
+                                    second_c = true;
+                                    w = brake.now_word;
+                                    n_of_tests--;
+                                }
+                                n_of_tests++;
+                            }
+                           
+                           
+                            
+                            
+                            if(test)
+                            {
+                                try
+                                {
+                                    if (n_of_tests < words.Length)
+                                    {
+                                        if (second_c)
+                                        {
+                                            try
+                                            {
+
+
+                                                char[] tmp = brake.brakingC(w);
+                                                string r = String.Empty;
+                                                for (int i = 0; i < tmp.Length; i++)
+                                                {
+                                                    r += tmp[i] + " ";
+                                                }
+                                                await Bot.SendTextMessageAsync(message.Chat.Id, r);
+                                            }
+                                            catch (Exception)
+                                            {
+
+                                                await Bot.SendTextMessageAsync(message.Chat.Id, "error");
+                                            }
+                                            finally
+                                            {
+                                                await Bot.SendTextMessageAsync(message.Chat.Id, "Now insert word from this letters");
+                                                answer = true;
+                                            }
+                                        }
+                                        if (!second_c)
+                                        {
+                                            try
+                                            {
+                                                char[] tmp = brake.brakingC();
+                                                string r = String.Empty;
+                                                for (int i = 0; i < tmp.Length; i++)
+                                                {
+                                                    r += tmp[i] + " ";
+                                                }
+                                                await Bot.SendTextMessageAsync(message.Chat.Id, r);
+                                            }
+                                            catch (Exception)
+                                            {
+
+                                                await Bot.SendTextMessageAsync(message.Chat.Id, "error");
+                                                test = false;
+                                            }
+                                            finally
+                                            {
+                                                await Bot.SendTextMessageAsync(message.Chat.Id, "Now insert word from this letters");
+                                                answer = true;
+                                            }
+                                        }
+
+                                    }
+                                    else
+                                    {
+                                        answer = false;
+                                        n_of_tests = 0;
+                                        test = false;
+                                    }
+                                }
+                                catch(Exception)
+                                {
+                                    await Bot.SendTextMessageAsync(message.Chat.Id,"error");
+                                }
+                            }
+                        }
+                        offset = update.Id + 1;
+                    }
+                }
+            }
+            catch(Telegram.Bot.Exceptions.ApiRequestException)
+            { 
+
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var text = @token_box.Text;
+            if(text!=""&&!this.bw.IsBusy)
+            {
+                this.bw.RunWorkerAsync(text);
+            }
+        }
+    }
+}
